00b0cbd6a8eafa2f791d68323edd1cffc6ccf3ea
Hiru5han <88285901+Hiru5han@users.noreply.github.com>
2024-08-25 10:26:04 +0100

    debug on

--- instance/config.py
+++ instance/config.py
@@ -3,3 +3,5 @@ import os
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///helpdesk.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+DEBUG = True
