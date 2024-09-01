3a07bce4d73108627bb56cbd6e0dd6f51a330e9b
Hiru5han <88285901+Hiru5han@users.noreply.github.com>
2024-09-01 09:37:41 +0100

    adding active ticket notification

--- app/routes.py
+++ app/routes.py
@@ -167,7 +167,18 @@ def all_tickets():
         Rendered template for the all_tickets page with all tickets.
     """
     tickets = Ticket.query.all()  # or your logic to get all tickets
-    return render_template('all_tickets.html', tickets=tickets, view='all')
+    open_tickets_count = Ticket.query.filter(Ticket.status.in_(['open', 'in progress'])).count()
+
+    if open_tickets_count > 10:
+        badge_class = "badge-active-tickets-high"  # Red
+    elif open_tickets_count > 5:
+        badge_class = "badge-active-tickets-medium"  # Yellow
+    elif open_tickets_count > 0:
+        badge_class = "badge-active-tickets-low"  # Green
+    else:
+        badge_class = "badge-active-tickets-info"  # Blue or default color
+
+    return render_template('all_tickets.html', tickets=tickets, view='all', open_tickets_count=open_tickets_count, badge_class=badge_class)
 
 
 @bp.route("/create_ticket", methods=["GET", "POST"])
